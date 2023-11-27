547f89ceabd6b780a878bc41968a197ebeefbf2c
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-11-27 10:42:54 +0530

    2D-Array

---
+++ Array2.c
@@ -0,0 +1,79 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+ main() {
+// int arr[3][3] = {{4,6,8},
+//                 {6,2,4}, 
+//                 {7,3,1}  
+// };
+
+// for(int i = 0 ; i <= 2 ; i++){
+//     for(int j = 0 ; j <= 2 ; j++){
+//         printf("%d", arr[i][j]);
+//     }
+//     printf("\n");
+// }
+
+
+// int arr[3][3];
+
+// for(int i = 0 ; i <= 2 ; i++){
+//     for(int j = 0 ; j <= 2 ; j++){
+//         printf("Enter value : ");
+//         scanf("%d",&arr[i][j]);
+//     }
+//     printf("New Row");
+//     printf("\n");
+// }
+
+// printf("Printing Array\n");
+
+// for(int i = 0 ; i <= 2 ; i++){
+//     for(int j = 0 ; j <= 2 ;j++){
+//         printf("%d",arr[i][j]);
+//     }
+//     printf("\n");
+// }
+
+// int arr[3][3] = {
+//     {2,4,1},{3,5,4},{8,2,6}
+// };
+
+// int sum = 0;
+// for(int i = 0 ; i <= 2 ; i++){
+//     for(int j = 0 ; j <= 2 ; j++){
+//         sum += arr[i][j];
+       
+//     }
+    
+// }
+//  printf("%0.2f",(float) sum/9);
+
+
+// int arr1[3][3] = { {2,4,1},{3,5,4},{8,2,6}};
+// int arr2[3][3] = {{3,6,2},{5,6,8},{3,7,4}};
+// int ans[3][3];
+
+// for(int i = 0  ; i <= 2 ; i++){
+//     for(int j = 0 ; j <= 2 ; j++){
+//      ans[i][j] = arr1[i][j] + arr2[i][j];
+//      printf("%d ",ans[i][j]);
+//     }
+//     printf("\n");
+// }
+
+
+
+//  int arr[3][3] = { {2,4,1},{3,5,4},{8,2,6}};
+ 
+//  int sum = 0;
+//  for(int i = 0 ; i <= 2 ; i++){
+//      for(int j = 0 ; j <= 2 ; j++){
+//          if(i == j){
+//              sum +=arr[i][j];
+//          }
+  
+//      }
+//  }
+// printf("%d",sum);
+}
